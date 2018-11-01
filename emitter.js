077b1f4b58458ad797951a1d21a2c05a4ce39442
Shchupakov Antony <a.shchupakov@mail.ru>
2018-11-01 15:36:57 +0500

    Implement 'off' method

--- emitter.js
+++ emitter.js
@@ -6,6 +6,12 @@
  */
 const isStar = true;
 
+function isSubEvent(event, subEvent) {
+    return event.startsWith(subEvent) ||
+           event.endsWith(subEvent) ||
+           event.includes(`.${subEvent}.`);
+}
+
 /**
  * Возвращает новый emitter
  * @returns {Object}
@@ -38,7 +44,18 @@ function getEmitter() {
          * @returns {Object} this
          */
         off: function (event, context) {
-            console.info(event, context);
+            const innerListenedEvents = this.listenedEvents; // Чтобы не возиться с this в forEach
+
+            Object.keys(innerListenedEvents).forEach(function (eventName) {
+                const isEventMatches = isSubEvent(eventName, event);
+                if (isEventMatches) {
+                    innerListenedEvents[eventName]
+                        .filter(subscription => subscription.context === context)
+                        .forEach(function (subscription) {
+                            subscription.repeat = 0;
+                        });
+                }
+            });
 
             return this;
         },
