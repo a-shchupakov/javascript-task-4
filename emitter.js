71d57afb8dfff869fe059a7bd4aa95633939ebac
Shchupakov Antony <a.shchupakov@mail.ru>
2018-11-01 15:14:50 +0500

    Implement several methods

--- emitter.js
+++ emitter.js
@@ -12,32 +12,59 @@ const isStar = true;
  */
 function getEmitter() {
     return {
+        listenedEvents: {},
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @param {Number} repeat
+         * @returns {Object} this
          */
-        on: function (event, context, handler) {
-            console.info(event, context, handler);
+        on: function (event, context, handler, repeat = Infinity) {
+            if (typeof this.listenedEvents[event] === 'undefined') {
+                this.listenedEvents[event] = [];
+            }
+            this.listenedEvents[event].push({ context, handler, repeat });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} this
          */
         off: function (event, context) {
             console.info(event, context);
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} this
          */
         emit: function (event) {
-            console.info(event);
+            const subscriptions = this.listenedEvents[event];
+            if (typeof subscriptions !== 'undefined') {
+                subscriptions
+                    .filter(subscription => subscription.repeat > 0)
+                    .forEach(function (subscription) {
+                        subscription.handler.apply(subscription.context);
+                        subscription.repeat--;
+                    });
+            }
+
+            const dotIndex = event.lastIndexOf('.');
+            if (dotIndex !== -1) {
+                this.emit(event.substring(0, dotIndex));
+            }
+
+            return this;
         },
 
         /**
@@ -47,9 +74,13 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object} this
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            times = times <= 0 ? Infinity : times;
+            this.on(event, context, handler, times);
+
+            return this;
         },
 
         /**
@@ -59,9 +90,24 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object} this
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            if (frequency > 0) {
+                var counter = 0;
+                const newHandler = function () {
+                    if (counter++ % frequency === 0) {
+                        handler.apply(this);
+                    }
+                };
+                this.on(event, context, newHandler);
+
+                return this;
+            }
+
+            this.on(event, context, handler);
+
+            return this;
         }
     };
 }
